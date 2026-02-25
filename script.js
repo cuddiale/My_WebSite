27f640a3e9abb5bd9ff853b3c6212ff837c77a1f
alessio cuddretto <alessiocuddretto@hotmail.com>
2026-02-25 17:24:28 +0100

    aggiornamenti sitoweb

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const titles = [
-  "Ingegnere Informatico",
-  "Full Stack Developer",
-  "UX/UI Designer",
-];
+// const titles = [
+//   "Ingegnere Informatico",
+//   "Full Stack Developer",
+//   "UX/UI Designer",
+// ];
 
-let index = 0;
-const textElement = document.getElementById("rotating-text");
+// let index = 0;
+// const textElement = document.getElementById("rotating-text");
 
-setInterval(() => {
-  textElement.style.opacity = 0;
+// setInterval(() => {
+//   textElement.style.opacity = 0;
 
-  setTimeout(() => {
-    index = (index + 1) % titles.length;
-    textElement.textContent = titles[index];
-    textElement.style.opacity = 1;
-  }, 500);
-}, 3000);
+//   setTimeout(() => {
+//     index = (index + 1) % titles.length;
+//     textElement.textContent = titles[index];
+//     textElement.style.opacity = 1;
+//   }, 500);
+// }, 3000);
